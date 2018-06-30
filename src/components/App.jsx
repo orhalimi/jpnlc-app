2514f3e77fca199304a570e89841c89ad71447d5
Or Halimi <or50002001@gmail.com>
2018-06-30 18:18:36 +0300

    edit heroku config

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -10,7 +10,7 @@ class App extends Component {
       <div className="App">
         <div className="container">
           <Header />
-          {console.log(process.env.NODE_ENV)}
+          {console.log(process.env)}
           <Content />
         </div>
       </div>
