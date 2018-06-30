1c1e31439319da9c9e1920de441ed45c19ab8e9d
Or Halimi <or50002001@gmail.com>
2018-06-30 17:55:07 +0300

    edit heroku config

--- src/util/requests.js
+++ src/util/requests.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-import config from '../confige/dev';
+import config from '../confige/config';
 
 export const getAvailableForms = async () => {
   const availableForms = await axios.get(`${config.apiPath}/available_forms`);
